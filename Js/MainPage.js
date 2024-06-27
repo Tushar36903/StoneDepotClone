499b1a9a9558d26f42d21e7610d5c4934dbb2035
tushar36903 <redhand1630@gmail.com>
2024-06-27 10:36:50 +0530

    first commit

---
+++ Js/MainPage.js
@@ -0,0 +1,21 @@
+const DB = document.getElementById("downloadButton");
+const dT = document.getElementById("downloadTimer");
+const da = document.getElementById("downloadafter");
+
+let timer;
+let countValue = 5;
+
+DB.addEventListener("click", function () {
+    DB.style.display = "none";
+
+    timer = setInterval(function () {
+        if (countValue <= 0) {
+            clearInterval(timer);
+            dT.innerHTML = "";
+            da.style.display = "block";
+        } else {
+            dT.innerHTML = `Starting download in ${countValue} seconds...`;
+        }
+        countValue--;
+    }, 500);
+});
