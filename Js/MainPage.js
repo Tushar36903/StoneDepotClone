58dc48623a25888dae2315d9ef1231936b9d86d9
Tushar7564 <tusharsinghal2703@gmail.com>
2024-06-28 16:24:50 +0530

    Updated multiple HTML, CSS, and JS files; added new images

--- Js/MainPage.js
+++ Js/MainPage.js
@@ -1,21 +1,21 @@
-const DB = document.getElementById("downloadButton");
-const dT = document.getElementById("downloadTimer");
-const da = document.getElementById("downloadafter");
+// const DB = document.getElementById("downloadButton");
+// const dT = document.getElementById("downloadTimer");
+// const da = document.getElementById("downloadafter");
 
-let timer;
-let countValue = 5;
+// let timer;
+// let countValue = 5;
 
-DB.addEventListener("click", function () {
-    DB.style.display = "none";
+// DB.addEventListener("click", function () {
+//     DB.style.display = "none";
 
-    timer = setInterval(function () {
-        if (countValue <= 0) {
-            clearInterval(timer);
-            dT.innerHTML = "";
-            da.style.display = "block";
-        } else {
-            dT.innerHTML = `Starting download in ${countValue} seconds...`;
-        }
-        countValue--;
-    }, 500);
-});
+//     timer = setInterval(function () {
+//         if (countValue <= 0) {
+//             clearInterval(timer);
+//             dT.innerHTML = "";
+//             da.style.display = "block";
+//         } else {
+//             dT.innerHTML = `Starting download in ${countValue} seconds...`;
+//         }
+//         countValue--;
+//     }, 500);
+// });
